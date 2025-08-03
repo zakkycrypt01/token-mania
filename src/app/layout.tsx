00fb941964aab01b46d612835ecec0e1f114d17c
Zakariyah <zakariyahayomide01@gmail.com>
2025-08-03 20:37:04 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type {Metadata} from 'next';
 import './globals.css';
+import '@solana/wallet-adapter-react-ui/styles.css';
 import { Toaster } from "@/components/ui/toaster"
 import { cn } from '@/lib/utils';
 import WalletProvider from '@/components/providers/wallet-provider';
--- src/components/providers/wallet-provider.tsx
+++ src/components/providers/wallet-provider.tsx
@@ -11,8 +11,6 @@ import {
 } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
 
-require('@solana/wallet-adapter-react-ui/styles.css');
-
 const WalletProvider: FC<{ children: React.ReactNode }> = ({ children }) => {
     const network = WalletAdapterNetwork.Devnet;
 
