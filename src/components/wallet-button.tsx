8ceae4618ee59792167c4efed417c27af7217803
Zakariyah <zakariyahayomide01@gmail.com>
2025-08-03 20:54:48 +0000

    use the wallet connect buton color throughtoutn

--- src/components/wallet-button.tsx
+++ src/components/wallet-button.tsx
@@ -4,9 +4,10 @@ import { useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { Wallet } from "lucide-react";
 import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
 
 const WalletButton = () => {
-    const { wallet, publicKey } = useWallet();
+    const { wallet, publicKey, connect, select, connecting, disconnecting } = useWallet();
     const [isClient, setIsClient] = useState(false);
 
     useEffect(() => {
@@ -18,11 +19,19 @@ const WalletButton = () => {
         : "Connect Wallet";
 
     if (!isClient) {
-        return null;
+        return (
+             <Button variant="default" disabled={true}>
+                <Wallet className="mr-2 h-4 w-4" />
+                Connect Wallet
+            </Button>
+        );
     }
 
     return (
-        <WalletMultiButton style={{}}>
+        <WalletMultiButton style={{ 
+            '--wallet-adapter-button-background-color': 'hsl(var(--primary))',
+            '--wallet-adapter-button-hover-color': 'hsl(var(--primary) / 0.9)',
+         } as React.CSSProperties}>
             <Wallet className="mr-2 h-4 w-4" />
             {wallet ? formattedAddress : "Connect Wallet"}
         </WalletMultiButton>
