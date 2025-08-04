b48cb1660efa274f59b7dae248a774fa22feaf34
Zakariyah <zakariyahayomide01@gmail.com>
2025-08-04 08:30:30 +0000

    use the original discord logo

--- src/components/sections/community.tsx
+++ src/components/sections/community.tsx
@@ -1,6 +1,22 @@
 import { Button } from "@/components/ui/button";
-import { Twitter, Send, Users } from "lucide-react";
+import { Twitter, Send } from "lucide-react";
 import Link from "next/link";
+import type { SVGProps } from "react";
+
+function DiscordIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+    >
+      <path d="M20.317 4.36981C18.824 3.79441 17.252 3.34561 15.608 3.01801C15.422 3.32401 15.253 3.71961 15.099 4.03881C13.237 3.74361 11.411 3.74361 9.54901 4.03881C9.39501 3.71961 9.22501 3.32401 9.04001 3.01801C7.39601 3.34561 5.82401 3.79441 4.33101 4.36981C1.67401 9.17641 1.07701 13.7272 1.88901 18.1056C3.64901 19.1668 5.46101 19.8408 7.32901 20.3232C7.68901 19.8644 8.01701 19.3784 8.32901 18.8664C7.47101 18.5208 6.64901 18.1292 5.86901 17.684C6.01301 17.5856 6.15101 17.4812 6.28901 17.3708C10.435 19.1432 14.213 19.1432 18.357 17.3708C18.495 17.4812 18.633 17.5856 18.777 17.684C17.997 18.1292 17.175 18.5208 16.317 18.8664C16.629 19.3784 16.957 19.8644 17.317 20.3232C19.185 19.8408 20.997 19.1668 22.757 18.1056C23.641 13.0336 22.735 8.44121 20.317 4.36981ZM12.015 15.1236C10.747 15.1236 9.69101 14.052 9.69101 12.7636C9.69101 11.4752 10.733 10.4036 12.015 10.4036C13.297 10.4036 14.353 11.4752 14.339 12.7636C14.339 14.052 13.297 15.1236 12.015 15.1236ZM16.383 15.1236C15.115 15.1236 14.059 14.052 14.059 12.7636C14.059 11.4752 15.101 10.4036 16.383 10.4036C17.665 10.4036 18.721 11.4752 18.707 12.7636C18.707 14.052 17.665 15.1236 16.383 15.1236Z" />
+    </svg>
+  );
+}
 
 export default function CommunitySection() {
   return (
@@ -28,7 +44,7 @@ export default function CommunitySection() {
             </Button>
             <Button size="lg" asChild className="bg-indigo-500 hover:bg-indigo-600 text-white">
               <Link href="#" target="_blank" rel="noopener noreferrer">
-                <Users className="mr-2 h-5 w-5" />
+                <DiscordIcon className="mr-2 h-5 w-5" />
                 Discord
               </Link>
             </Button>
